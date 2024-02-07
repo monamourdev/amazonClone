e562a85da4b12b437822f62cfcfcb5745c8e5904
monamour.dev <tiagosardi@gmail.com>
2024-02-07 20:57:30 -0300

    Feat: Create firebaseConfig file

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD3auWQuTz35Ja5sW4JF9ffa5B4P1Bl1ms",
+    authDomain: "challenge-de6b3.firebaseapp.com",
+    projectId: "challenge-de6b3",
+    storageBucket: "challenge-de6b3.appspot.com",
+    messagingSenderId: "700722014363",
+    appId: "1:700722014363:web:469a4f462f4759d1527e3e",
+    measurementId: "G-97ND0DHEGZ"
+  };
