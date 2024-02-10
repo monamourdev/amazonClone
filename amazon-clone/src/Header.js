1f641b5774a2d04b5d87f9efa02e21770970e197
monamourdev <tiagomendonca.dev@gmail.com>
2024-02-09 23:12:31 -0300

    feat: Create router to basket and home

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -2,13 +2,16 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className='header'>
-            <img
-                className='header__logo'
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon logo" />
+            <Link to="/">
+                <img
+                    className='header__logo'
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon logo" />
+            </Link>
 
             <div
                 className='header__search'>
@@ -42,12 +45,14 @@ function Header() {
                         Prime
                     </span>
                 </div>
-                <div className='header__optionBasket'>
-                    <ShoppingBasketIcon />
-                    <span className='header__optionLineTwo header__basketCount'>
-                        0
-                    </span>
-                </div>
+                <Link to="/checkout" >
+                    <div className='header__optionBasket'>
+                        <ShoppingBasketIcon />
+                        <span className='header__optionLineTwo header__basketCount'>
+                            0
+                        </span>
+                    </div>
+                </Link>
             </div>
         </div>
     )
