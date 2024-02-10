3e4403629f690fafa36781c1dda50b7cc770ad29
monamourdev <tiagomendonca.dev@gmail.com>
2024-02-09 22:39:57 -0300

    feat: Create first routes to home and checkout

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,13 +2,19 @@
 import './App.css';
 import Header from './Header'
 import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Home />
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/checkout" element={
+          <p>CHECKOUT BABY</p>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
